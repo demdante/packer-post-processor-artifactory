1b6bd73e1dc7a884fc4ed1046ed2ae2563a94b6a
john <64720591+demdante@users.noreply.github.com>
2022-01-08 01:47:17 -0500

    change

--- artifactory/post-processor.go
+++ artifactory/post-processor.go
@@ -1,6 +1,6 @@
 //go:generate mapstructure-to-hcl2 -type Config
 
-package main
+package artifactory
 
 import (
 	"bufio"
@@ -20,10 +20,10 @@ import (
 	"strings"
 
 	"github.com/hashicorp/hcl/v2/hcldec"
-	"github.com/hashicorp/packer/common"
-	"github.com/hashicorp/packer/helper/config"
-	"github.com/hashicorp/packer/packer"
-	"github.com/hashicorp/packer/template/interpolate"
+	common "github.com/hashicorp/packer-plugin-sdk/common"
+	"github.com/hashicorp/packer-plugin-sdk/packer"
+	"github.com/hashicorp/packer-plugin-sdk/template/config"
+	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
 )
 
 type Config struct {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/pyToshka/packer-post-processor-artifactory
 go 1.13
 
 require (
-	github.com/hashicorp/hcl/v2 v2.2.0
-	github.com/hashicorp/packer v1.5.1
-	github.com/zclconf/go-cty v1.2.0
+	github.com/hashicorp/hcl/v2 v2.8.0
+	github.com/hashicorp/packer-plugin-sdk v0.0.14
+	github.com/zclconf/go-cty v1.7.0
 )
